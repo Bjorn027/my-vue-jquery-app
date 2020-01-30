cee21ba50ae0a19a0f645f45de9818f420b34e7a
shawn <shawn.boreland@easternstudent.ca>
2020-01-30 14:23:37 -0400

    group and notes lists added but not showing with the for loop

--- src/components/Groups.vue
+++ src/components/Groups.vue
@@ -1,10 +1,27 @@
 <template>
-  <v-card class="mx-auto" height="800" max-width="800" outlined>
-    <v-btn color="#800000" @click="getGroups()">Groups</v-btn>
-    <v-btn color="#800000" @click="createGroup()">Create Group</v-btn>
-    <input v-model="createGroupText" placeholder="name" />
-    <v-btn color="#800000" @click="removeGroup()">Remove Group</v-btn>
-  </v-card>
+ <div> 
+      <section>
+        <h4>Groups</h4>
+        <input v-model="createGroupText" placeholder="name">
+        <button @click="createGroup">Create Group</button>
+        <br>
+        <input type="radio" name="group" v-model="groupId" value="" @click="getNotes()" /> All
+        <div v-for="group in groups" :key="group._id">
+          <input type="radio" name="group" v-model="groupId" :value="group._id" @click="getNotes(group._id)" />
+          {{group.text}}
+          <button class="delete" @click="removeGroup(group._id)">Delete Group</button>
+        </div>
+      </section>
+
+      <section>
+        <h4>Notes</h4>
+        <textarea v-model="createNoteText" placeholder="note" cols="100" rows="5"></textarea><br>
+        <button @click="createNote">Create Note</button>
+        <div v-for="note in notes" :key="note._id" class="note">
+          {{note.text}}
+          <button class="delete" @click="removeNote(note._id)">Delete Note</button>
+        </div>
+      </section></div>
 </template>
 
 <script>
@@ -32,6 +49,58 @@ export default {
         }
       });
     },
+    getNotes(groupId) {
+    $.get(server + "/notes", { groupId }, res => {
+      if (res.success) {
+        this.notes = res.notes;
+      } else {
+        this.res = res;
+      }
+    });
+  },
+  createNote() {
+    $.post(
+      server + "/notes/create",
+      {
+        text: this.createNoteText,
+        groupId: this.groupId
+      },
+      res => {
+        if (res.success) {
+          this.notes = res.notes;
+          this.createNoteText = "";
+        } else {
+          this.res = res;
+        }
+      }
+    );
+  },
+  logout() {
+      $.post(server + "/logout", res => {
+        if (res.success) {
+          this.loggedIn = false;
+          this.username = "";
+          this.createGroupText = "";
+          this.createNoteText = "";
+          this.notes = [];
+          this.groups = [];
+          this.groupId = "";
+        }
+      });
+    
+  
+  },
+  removeNote(noteId) {
+    console.log({ noteId });
+    $.post(server + "/notes/remove", { noteId }, res => {
+      if (res.success) {
+        this.getNotes();
+      } else {
+        this.res = res;
+      }
+    });
+  }
+},
     createGroup() {
       $.post(
         server + "/groups/create",
@@ -48,7 +117,7 @@ export default {
         }
       );
     }
-  }
+  
 };
 </script>
 
